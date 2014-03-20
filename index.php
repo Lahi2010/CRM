51b381ef57aa427fc6a2cddd05b537ab1fad41c8
unknown <lahiru.c@FMF-INO-I5L2.fmf.local>
2014-03-20 11:12:33 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html  lang="en" class="no-js">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>findmyfare CRM</title>
+<link rel="shortcut icon" type="image/png" href="dist/images/favicon.png"/>
+
+<!-- Bootstrap -->
+<link href="dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="dist/fonts/Hand_font/stylesheet.css" rel="stylesheet" type="text/css">
+<link href="dist/css/tab.css" rel="stylesheet" type="text/css">
+<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="ie_style.css" />
+<![endif]-->
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<![endif]-->
+</head>
+
+<body>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="dist/js/bootstrap.js"></script>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="header">
+        <div class="logo"> <a href="index.php"> <img src="dist/images/logo.png" /> </a> </div>
+        <div class="header_right">
+          <div class="header_tel"><span class="quote">Sri Lanka's Largest Online Travel Company</span><span><a href="#" class="header_tel_num"> +94(0) 117 24 7 365</a></span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="title_bar"> <img src="dist/images/crm_logo.png" width="33" height="30" />&nbsp;&nbsp;&nbsp;&nbsp;fmf CRM System</div>
+    </div>
+   
+  </div>
+</div>
+</div>
+<div class="container"> <div class="content_bg">
+  <div class="row">
+ 
+    <div class="col-md-12">
+    <div class="col-md-5  box_wrap">
+    <div class="center_circle"></div>
+      <div class="center_box test" >
+        
+    <div class="control-label">Username</div>
+     <div class="input-group">
+  <span class="input-group-addon"><img src="dist/images/username_img.png" width="20" height="20" /></span>
+  <input type="text" class="form-control" placeholder="Username">
+</div>
+
+<div class="control-label set_margin_top_10">Password</div>
+     <div class="input-group">
+  <span class="input-group-addon"><img src="dist/images/password_img.png" width="20" height="20"/></span>
+  <input type="password" class="form-control" placeholder="Password">
+</div>
+
+<div class="input-group set_full_width">
+
+<button type="button" class="btn btn-default set_margin_top_10  pull-right" >Sign in</button>
+
+</div>
+<div class="clearfix"></div>
+<div class="input-group set_full_width set_margin_top_10">
+
+<div class="text-right border-right col-xs-6"><a id="lnk_fp" class="login_lnk" href="#">Sign up</a></div>
+<div class="col-xs-6"><a id="lnk_rg"  class="login_lnk" href="#">Forgot password</a></div>
+
+</div>
+<div class="clearfix"></div>
+        </div>
+        </div>
+    </div>
+  </div>
+  </div>
+</div>
+
+<div class="container"><div class="row">
+<div class="col-md-12">
+<div class="sponsers">
+
+<div class="col-md-2 sponser_item item_1"></div>
+<div class="col-md-2 sponser_item item_2"></div>
+<div class="col-md-2 sponser_item item_3"></div>
+<div class="col-md-2 sponser_item item_4"></div>
+<div class="col-md-2 sponser_item item_5"></div>
+<div class="col-md-2 sponser_item item_6"></div>
+<div class="clearfix"></div> 
+</div>
+</div></div></div>
+
+
+<div class="container"><div class="row">
+<div class="col-md-12">
+<div class="footer">
+</div>
+</div></div></div>
+
+<!-- footer links-->
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<!-- tab-->
+<div class="footer_tab">
+<div class="footer_tab_wrapper">
+  <ul class="tabs footer_tab_style" >
+  <li rel="tab1"><img src="dist/images/home_icon.png" width="19" height="17" />&nbsp; FMF CRM</li>
+  <li rel="tab2" class="active" ><img src="dist/images/create_icon.png" width="19" height="19" />&nbsp;  Create</li>
+  <li rel="tab3"><img src="dist/images/view_icon.png" width="19" height="19" /> &nbsp; View</li>
+  <li rel="tab4"><img src="dist/images/other_icon.png" width="19" height="19" /> &nbsp; Other</li>
+  <li rel="tab5"><img src="dist/images/about_icon.png" width="19" height="19" /> &nbsp; About</li>
+ 
+   
+
+
+
+ 
+ 
+</ul>
+  </div>
+<div class="tab_container_footer">
+  <h5 class="tab_drawer_heading" rel="tab1"><img src="dist/images/home_icon.png" width="19" height="19" />&nbsp; FMF CRM</h5>
+  <div id="tab1" class="tab_content_footer" >
+  <h4>findmyfare CRM System version 1.0 beta</h4>
+    <p style="font-size:12px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac metus augue.</p>
+  </div>
+  <!-- #tab1 -->
+  <h5 class="d_active tab_drawer_heading d_active" rel="tab2"><img src="dist/images/create_icon.png" width="19" height="19" />&nbsp;  Create</h5>
+  <div id="tab2" class="tab_content_footer" style="display:block;">
+
+  
+    <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/invoice_icon.png" width="23" height="28"/>Flights Invoice </a></div>
+     <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/booking_icon.png" width="27" height="26"/>Hotel Booking </a></div>
+     
+       
+       <div class="clearfix"></div>
+        
+  </div>
+  <!-- #tab2 -->
+  <h5 class="tab_drawer_heading" rel="tab3"><img src="dist/images/view_icon.png" width="19" height="19" /> &nbsp; View</h5>
+  <div id="tab3" class="tab_content_footer">
+ 
+   
+   <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Common </a></div>
+     <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Beatmyfare </a></div>
+      <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Getmyfare </a></div>
+       <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Flights </a></div>
+      
+       <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Hotels </a></div>
+     <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Inter Hotels </a></div>
+      <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/grid_icon.png" width="28" height="28"/>Budget Flights </a></div>
+       
+       <div class="clearfix"></div>
+   
+   
+  </div>
+  <!-- #tab3 -->
+  <h5 class="tab_drawer_heading" rel="tab4"><img src="dist/images/other_icon.png" width="19" height="19" /> &nbsp; Other</h5>
+  <div id="tab4" class="tab_content_footer">
+   
+   <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/creditcard_icon.png" width="27" height="18"/>Credit Card </a></div>
+     <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/cash_icon.png" width="18" height=""/>Payments-Cash </a></div>
+      <div class="col-md-2"><a href="#" class="tab_button"><img src="dist/images/issue_icon.png" width="20" height="27"/>Flights Issue </a></div>
+   
+       
+       <div class="clearfix"></div>
+  </div>
+  <!-- #tab4 --> 
+  
+  <h5 class="tab_drawer_heading" rel="tab5"><img src="dist/images/about_icon.png" width="19" height="19" /> &nbsp; About</h5>
+  <div id="tab5" class="tab_content_footer">
+  <h4>About</h4>
+    <p>Integer ultrices lacus sit amet lorem viverra consequat. Vivamus lacinia interdum sapien non faucibus. Maecenas bibendum, lectus at ultrices viverra, elit magna egestas magna, a adipiscing mauris justo nec eros.</p>
+  </div>
+  <!-- #tab5 --> 
+  
+
+  
+  
+ 
+</div>
+<!-- .tab_container_footer -->
+ <script src="dist/js/tab_footer.js"></script> 
+    <!-- tab--> 
+</div><!-- .footer_tab -->
+</div>
+
+ 
+</div>
+</div>
+</body>
+</html>
