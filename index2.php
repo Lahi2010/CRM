064ba6844808e4e94aa61c2396c2aa608debec78
unknown <lahiru.c@FMF-INO-I5L2.fmf.local>
2014-03-25 16:05:38 +0530

    finished footer links

--- index2.php
+++ index2.php
@@ -156,7 +156,9 @@
 <!-- footer links-->
 <div class="container">
 <div class="row">
-<div class="col-md-12">
+
+<div class="footer_more_links">
+<div class="col-md-12 set_height_186">
 <!-- tab-->
 <div class="footer_tab">
 <div class="footer_tab_wrapper">
@@ -352,37 +354,37 @@
   <h5 class="tab_drawer_heading_footer" rel="tab5_footer"><img src="dist/images/about_icon.png" width="19" height="19" /> &nbsp; About</h5>
   <div id="tab5_footer" class="tab_content_footer">
   <h4>Holiday Packages</h4>
-      <div class="col-sm-2">
+      <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
  
-    </div>   <div class="col-sm-2">
+    </div>   <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
 
-    </div>   <div class="col-sm-2">
+    </div>   <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
 
-    </div>   <div class="col-sm-2">
+    </div>   <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
 
-    </div>   <div class="col-sm-2">
+    </div>   <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     
-    </div>   <div class="col-sm-2">
+    </div>   <div class="col-md-2">
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
     <div><a href="#">Demo link 1</a></div>
@@ -402,9 +404,117 @@
     <!-- tab--> 
 </div><!-- .footer_tab -->
 </div>
+</div>
+ <div class="clearfix"></div>
 
- 
+
+</div></div>
+
+
+<div class="container">
+<div class="row">
+
+<div class="col-md-12" >
+<div class="footer_links_2">
+<div class="col-md-4">
+<img src="dist/images/logo_small.png" >
+<p>
+Findmyfare.com allows you to search, compare and select the flight you want with a just a few clicks. Plan your dream trip with affordable fares, utmost convenience and freedom from the conscious service provider. Findmyfare.com also pioneers in providing innovative travel solutions.
+</p>
+</div>
+<div class="col-md-2">
+<div class="foot_link_group_title"><h6><strong>findmyfare</strong></h6></div>
+<div class="footer_link_2_group">
+<div><a href="">About us</a></div>
+<div><a href="">Partners</a></div>
+<div>
+<a href="">Certified by</a></div>
+<div><a href="">Investor Relations</a></div>
+<div><a href="">FAQ’s</a></div>
+<div><a href="">Privacy Policy</a></div>
+<div><a href="">Contact Us</a></div>
+<div><a href="">Terms & conditions</a></div>
+<div><a href="">Testimonials</a></div>
+<div><a href="">Careers</a></div>
+</div>
+
+</div>
+<div class="col-md-2">
+<div class="foot_link_group_title"><h6><strong>Our products</strong></h6></div>
+<div class="footer_link_2_group">
+<div><a href="">Flights</a></div>
+<div><a href="">Srilankan Hotels</a></div>
+<div>
+<a href="">Hot deals on Hotels</a></div>
+<div><a href="">Day Outing</a></div>
+<div><a href="">Early-Bird Deals</a></div>
+<div><a href="">International Hotels</a></div>
+<div><a href="">Travel Insurance</a></div>
+<div><a href="">Traveler SIM</a></div>
+
 </div>
 </div>
+<div class="col-md-2">
+<div class="foot_link_group_title"><h6><strong>Payment information</strong></h6></div>
+<div class="footer_link_2_group">
+<div><a href="">Bank Transfer</a></div>
+<div><a href="">Credit Card</a></div>
+<div><a href="">Monthly Payment</a></div>
+<div><a href="">Pay Cash</a></div>
+<div><a href="">Door Collection</a></div>
+<div><a href="">Corporate Travel</a></div>
+<div><a href="">Sub-agent model</a></div>
+
+
+</div>
+</div>
+<div class="col-md-2">
+<div class="foot_link_group_title"><h6><strong>Other information</strong></h6></div>
+<div class="footer_link_2_group">
+<div><a href="">Visa Information</a></div>
+<div><a href="">Premium Airlines</a></div>
+<div><a href="">Budget Airlines</a></div>
+<div><a href="">Airlines Web Check-in</a></div>
+<div><a href="">Srilankan Airlines</a></div>
+<div><a href="">Singapore Airlines</a></div>
+<div><a href="">Malaysian Airlines</a></div>
+<div><a href="">Air India</a></div>
+<div><a href="">Jet Airways</a></div>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+</div></div></div> <!-- linkes grup2 -->
+
+<div class="container"><div class="row">
+<div class="col-md-12">
+<div class="sponsers">
+
+<div class="col-md-2 sponser_item item_1"></div>
+<div class="col-md-2 sponser_item item_2"></div>
+<div class="col-md-2 sponser_item item_3"></div>
+<div class="col-md-2 sponser_item item_4"></div>
+<div class="col-md-2 sponser_item item_5"></div>
+<div class="col-md-2 sponser_item item_6"></div>
+<div class="clearfix"></div> 
+</div>
+</div></div></div><!-- spnosors -->
+
+<div class="container">
+<div class="row">
+
+<div class="col-md-12" >
+<div class="footer_below_wrap">
+<div class="col-md-4">
+&nbsp;© FindMyFare 2014. All right reserved.
+</div>
+<div class="col-md-8">
+<div class="footer_sub_links_wrap">
+<a href="#">About Us</a><a href="#"> FAQ</a><a href="#"> Privacy</a><a href="#"> Security</a><a href="#"> Terms and Condition</a>
+</div>
+</div>
+<div class="clearfix"></div>
+</div>
+</div></div></div> <!-- all rights -->
 </body>
 </html>
